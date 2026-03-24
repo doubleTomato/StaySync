1371fa1ae9d767764cf7a0aa6e3fad2aa0155c69
doubleTomato <doubleTomato21@gmail.com>
2026-03-24 16:03:41 +0900

    [add] top헤더 디자인 및 leftfilter 위치 변경

--- staysync/src/components/common/topHeader.tsx
+++ staysync/src/components/common/topHeader.tsx
@@ -1,27 +1,39 @@
 'use client'
-import { MdOutlineSearch, MdOutlinePerson  } from "react-icons/md";
+import { MdOutlineSearch, MdOutlinePerson, MdArrowDropDown  } from "react-icons/md";
 import { useState } from 'react';
 import Image from 'next/image'
 export default function topHeader(){
     const [login, setLogined] = useState(false);
     return (
         <header className="flex items-center shadow-md justify-between gap-4 px-40 sticky top-0 bg-white z-10">
-            <div><Image src="/logo_2.png" alt="로고 사진" 
-             width={150} height={40} /></div>
-            <div className='bg-amber-50 sticky top-25 h-[70px] flex items-center justify-between shadow-lg'>
-                <ul className="flex">
-                    <li>
-                        <p></p>
-                        <p></p>
-                    </li>
-                    <li>날짜(datepicker)</li>
-                    <li>인원수(어른/아이) +- 카운트 형식</li>
-                </ul>
-                <p><MdOutlineSearch/></p>
-            </div>
             <div>
-                <MdOutlinePerson />
+                <Image src="/logo_2.png" alt="로고 사진" 
+                width={150} height={40} />
             </div>
+            <ul className="w-170 rounded-2xl border border-gray-300  flex items-center shadow-lg  overflow-hidden pr-2.5">
+                <li className="duration-700 rounded-2xl hover:bg-gray-200 pl-5 w-70 py-2.5 cursor-pointer">
+                    <p className="flex items-center gap-2.5 justify-between">
+                        <span className="text-gray-500 font-semibold text-base">지역</span>
+                        <span className="text-sm">서울시 경기도...</span>
+                        <span className="h-[15px] block bg-gray-300 w-[1px]"></span>
+                    </p>
+                </li>
+                 <li className="duration-700 rounded-2xl hover:bg-gray-200 pl-5 w-50 py-2.5 cursor-pointer">
+                    <p className="flex items-center gap-2.5 justify-between">
+                        <span className="text-gray-500 font-semibold text-base">날짜</span>
+                        <span className="text-sm">2025.05.21</span>
+                        <span className="h-[15px] block bg-gray-300 w-[1px]"></span>
+                    </p>
+                </li>
+                 <li className="duration-700 rounded-2xl hover:bg-gray-200 pl-5 w-40 py-2.5 cursor-pointer">
+                    <p className="flex items-center gap-2.5">
+                        <span className="text-gray-500 font-semibold text-base">인원수</span>
+                        <span className="text-sm">게스트 10명</span>
+                    </p>
+                </li>
+                 <li className="duration-700 hover:bg-gray-400 w-10 py-2.5 cursor-pointer rounded-4xl flex justify-center"><MdOutlineSearch/></li>
+            </ul>
+            <MdOutlinePerson />
         </header>
     )
 }
